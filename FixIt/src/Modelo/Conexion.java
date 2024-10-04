aece32f570d9d362ccbe0d360ee2f1b73dc4a519
Gabriel <gaboquintana10@gmail.com>
2024-10-04 11:49:22 -0600

    marca y modelo

--- FixIt/src/Modelo/Conexion.java
+++ FixIt/src/Modelo/Conexion.java
@@ -8,8 +8,8 @@ public class Conexion {
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
     //private static final String USUARIO = "jaimeu";
     //private static final String CONTRASENA = "qatar24";
-    private static final String USUARIO = "CHRISTIAN_MARIN";
-    private static final String CONTRASENA = "123456";
+    private static final String USUARIO = "FIXIT";
+    private static final String CONTRASENA = "FIXIT";
 
     public static Connection getConexion() {
         try {
