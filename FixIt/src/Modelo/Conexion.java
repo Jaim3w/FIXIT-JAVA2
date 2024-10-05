dc9c78ffce2c539d0b41984b78c12e783245e0b4
chaichor <christianpro313@ricaldone.edu.sv>
2024-10-05 15:23:28 -0600

    login acabado

--- FixIt/src/Modelo/Conexion.java
+++ FixIt/src/Modelo/Conexion.java
@@ -8,13 +8,8 @@ public class Conexion {
     private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
     //private static final String USUARIO = "jaimeu";
     //private static final String CONTRASENA = "qatar24";
-<<<<<<< HEAD
-    private static final String USUARIO = "FIXIT";
-    private static final String CONTRASENA = "FIXIT";
-=======
-    private static final String USUARIO = "fixa";
-    private static final String CONTRASENA = "qatar24";
->>>>>>> 3c3d0d9232d6954d4c2e0e60e29cc3c05a40641c
+    private static final String USUARIO = "CHRISTIAN_MARIN";
+    private static final String CONTRASENA = "123456";
 
     public static Connection getConexion() {
         try {
